0fc4610e157e9f007e19337c510a099cd2188468
Mariana Capelo <mariana.capelo@outsystems.com>
2021-06-30 19:17:25 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -244,7 +244,6 @@ $CAKE_EXE_INVOCATION = if ($IsLinux -or $IsMacOS) {
 # Build Cake arguments
 $cakeArguments = @("$Script");
 if ($Target) { $cakeArguments += "--target=$Target" }
-if ($Configuration) { $cakeArguments += "--configuration=$Configuration" }
 if ($Verbosity) { $cakeArguments += "--verbosity=$Verbosity" }
 if ($ShowDescription) { $cakeArguments += "--showdescription" }
 if ($DryRun) { $cakeArguments += "--dryrun" }
